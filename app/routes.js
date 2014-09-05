1e9e10e094c713af1d50045539e18e387455761a
Ron van der Wijngaard <rvdwijngaard@to-increase.com>
2014-09-05 12:43:30 +0200

    process the signup form

--- app/routes.js
+++ app/routes.js
@@ -52,6 +52,14 @@ module.exports = function(app, passport) {
 		req.logout();
 		res.redirect('/');
 	});
+
+	// process the signup form
+	app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect : '/profile', // redirect to the secure profile section
+		failureRedirect : '/signup', // redirect back to the signup page if there is an error
+		failureFlash : true // allow flash messages
+	}));
+	
 };
 
 // route middleware to make sure a user is logged in
