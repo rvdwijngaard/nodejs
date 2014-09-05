d7debdb44d0b7ba7b2b258f58b3e31b00cc36a7a
Ron van der Wijngaard <rvdwijngaard@to-increase.com>
2014-09-05 12:37:58 +0200

    initial version

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+	'url' : 'mongodb://rvdwijngaard:rvdwijngaard@proximus.modulusmongo.net:27017/ipevuW7i' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
